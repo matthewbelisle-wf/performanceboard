9f8b966f8325a9952f8a47f438ba5b7769e9f751
Matthew Belisle <matthew.belisle@webfilings.com>
2014-07-07 15:26:19 -0600

    Got recursive metric fetching, yay

--- app/metric.go
+++ app/metric.go
@@ -43,7 +43,33 @@ func (m *Metric) Save(c chan<- datastore.Property) error {
 	return datastore.SaveStruct(m, c)
 }
 
-// Put() recursively puts metrics into the datastore
+// Get() and Put() recursively gets/puts metrics into the datastore
+func (m *Metric) Get(c appengine.Context, key *datastore.Key) error {
+	q := datastore.NewQuery(MetricKind).Ancestor(key)
+	hierarchy := map[string]*Metric{} // {keyString: *metric}
+	for t := q.Run(c); ; {
+		child := Metric{}
+		childKey, err := t.Next(&child)
+		if err == datastore.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		hierarchy[childKey.Encode()] = &child
+	}
+	// Resetting hierarchy since ancestor filters limit results to the specified entity *and* its descendants.
+	hierarchy[key.Encode()] = m
+
+	for keyString, child := range hierarchy {
+		childKey, _ := datastore.DecodeKey(keyString)
+		parentKey := childKey.Parent()
+		if parent, ok := hierarchy[parentKey.Encode()]; ok {
+			parent.Children = append(parent.Children, *child)
+		}
+	}
+	return nil
+}
+
 func (m *Metric) Put(c appengine.Context, key *datastore.Key) error {
 	if _, err := datastore.Put(c, key, m); err != nil {
 		c.Errorf("Failed metric.Put()\nCould not do datastore.Put(): %s, %s", key.Encode(), err)
@@ -63,6 +89,25 @@ func (m *Metric) Put(c appengine.Context, key *datastore.Key) error {
 // HTTP handlers
 
 func getMetric(w http.ResponseWriter, r *http.Request) {
+	keyString := mux.Vars(r)["metric"]
+	key, err := datastore.DecodeKey(keyString)
+	if err != nil || key.Kind() != MetricKind {
+		http.Error(w, "Invalid metrics key: %s"+keyString, http.StatusBadRequest)
+		return
+	}
+	c := appengine.NewContext(r)
+	metric := Metric{}
+	if err = datastore.Get(c, key, &metric); err != nil {
+		http.Error(w, "Metric not found: "+keyString, http.StatusNotFound)
+		return
+	}
+	if err = metric.Get(c, key); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	JsonResponse{
+		"metric": metric,
+	}.Write(w)
 }
 
 func getMetrics(w http.ResponseWriter, r *http.Request) {

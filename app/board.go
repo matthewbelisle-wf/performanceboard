aaa7182e73d87a476a5af975506ef861d1075a25
Matt Belisle <mgbelisle@gmail.com>
2014-07-07 18:38:46 -0600

    Implemented board.Namespaces()

--- app/board.go
+++ app/board.go
@@ -14,73 +14,86 @@ type Board struct {
 	UserID string `datastore:"user_id"`
 }
 
-func (b *Board) Json(key *datastore.Key) (*Json, error) {
-	namespaces, err := b.Namespaces(key)
+func (b *Board) Json(c appengine.Context, key *datastore.Key) (*Json, error) {
+	namespaces, err := b.Namespaces(c, key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	return &Json{
 		"board":      key.Encode(),
-		"namespaces": b.Namespaces(key),
-	}
+		"namespaces": namespaces,
+	}, nil
 }
 
-// HTTP handlers
-
-func getBoard(w http.ResponseWriter, r *http.Request) {
-	boardKeyString := mux.Vars(r)["board"]
-	boardKey, err := datastore.DecodeKey(boardKeyString)
-	if err != nil {
-		http.Error(w, "Invalid boardKey: %s"+boardKeyString, http.StatusBadRequest)
-	}
+func (b *Board) Namespaces(c appengine.Context, key *datastore.Key) ([]*Namespace, error) {
 	q := datastore.NewQuery(MetricKind).
-		Ancestor(boardKey).
+		Ancestor(key).
 		Project("namespace").
 		Distinct()
-	c := appengine.NewContext(r)
-	namespaces := map[string]*Namespace{} // {metricKeyString: *namespace}
-	topNamespaces := []*Namespace{}
+	hierarchy := map[string]*Namespace{} // {metricKeyString: *namespace}
+	namespaces := []*Namespace{}
 	for t := q.Run(c); ; {
 		metric := Metric{}
 		metricKey, err := t.Next(&metric)
 		if err == datastore.Done {
 			break
+		} else if err != nil {
+			return nil, err
 		}
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		api, _ := router.Get("namespace").URL("board", boardKey.Encode(), "namespace", metric.Namespace)
-		namespaces[metricKey.Encode()] = &Namespace{
+		hierarchy[metricKey.Encode()] = &Namespace{
 			Name: metricKey.StringID(),
-			Api:  AbsURL(*api, r),
 		}
 	}
-	for metricKeyString, namespace := range namespaces {
+	for metricKeyString, namespace := range hierarchy {
 		key, _ := datastore.DecodeKey(metricKeyString)
 		if parentKey := key.Parent(); parentKey.Kind() == MetricKind {
-			parent, _ := namespaces[parentKey.Encode()]
+			parent, _ := hierarchy[parentKey.Encode()]
 			parent.Children = append(parent.Children, *namespace)
 		} else {
-			topNamespaces = append(topNamespaces, namespace)
+			namespaces = append(namespaces, namespace)
 		}
 	}
-	Json{
-		"namespaces": topNamespaces,
-	}.Write(w)
+	return namespaces, nil
 }
 
-func createBoard(writer http.ResponseWriter, request *http.Request) {
-	context := appengine.NewContext(request)
+// HTTP handlers
+
+func getBoard(w http.ResponseWriter, r *http.Request) {
+	keyString := mux.Vars(r)["board"]
+	key, err := datastore.DecodeKey(keyString)
+	if err != nil {
+		http.Error(w, "Invalid key string: %s"+keyString, http.StatusBadRequest)
+		return
+	}
+	c := appengine.NewContext(r)
 	board := Board{}
-	if u := user.Current(context); u != nil {
+	if err = datastore.Get(c, key, &board); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	json, err := board.Json(c, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.Write(w)
+}
+
+func createBoard(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	board := Board{}
+	if u := user.Current(c); u != nil {
 		board.UserID = u.ID
 	}
-	key, _ = datastore.Put(context, datastore.NewIncompleteKey(context, BoardKind, nil), &board)
-	json, err := board.Json(key)
+	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, BoardKind, nil), &board)
 	if err != nil {
-
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	api, _ := router.Get("board").URL("board", key.Encode())
-	json["api"] = AbsURL(*api, request)
+	json, err := board.Json(c, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	json.Write(w)
 }
--- app/namespace.go
+++ app/namespace.go
@@ -2,6 +2,5 @@ package performanceboard
 
 type Namespace struct {
 	Name     string      `json:"name"`
-	Api      string      `json:"api"`
 	Children []Namespace `json:"children"`
 }

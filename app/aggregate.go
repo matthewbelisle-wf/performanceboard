4500628ad56d2185b7235f30db55349631cb3704
Chad Bohannan <chad.bohannan@webfilings.com>
2014-08-08 13:17:52 -0600

    added start and end parameter support to aggregate endpoint

--- app/aggregate.go
+++ app/aggregate.go
@@ -27,6 +27,9 @@ func readAggregates(context appengine.Context,
 	boardKey *datastore.Key, namespace string, binType string,
 	newestTime time.Time, duration time.Duration,
 	limit int, cursor string) ([]AggregateMetric, error) {
+	// newestTime - same as 'end'
+	// duration - 0 if it should go forever, TODO::USE CURSOR!
+	// binTypes ["day", "hour", "minute", "second"]
 	q := datastore.NewQuery(AggregateMetricKind).
 		Filter("Namespace =", namespace).
 		Filter("BinType =", binType).
@@ -59,11 +62,34 @@ func getAggregates(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	namespace := mux.Vars(request)["namespace"]
-	binType := mux.Vars(request)["bin_type"]
+	binType := mux.Vars(request)["bin_type"] // day, hour, minute, second
 
-	// TODO parse begin and end time filters from url form parameters
+	end := time.Now()
+
+	// parse optional end time
+	if endParam := request.FormValue("end"); len(endParam) > 0 {
+		if end, err = time.Parse(time.RFC3339, endParam); err != nil {
+			context.Errorf("Error parsing end:%s:%v", endParam, err)
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	duration := time.Duration(0)
+
+	// parse optional start time
+	if startParam := request.FormValue("start"); len(startParam) > 0 {
+		if start, err := time.Parse(time.RFC3339, startParam); err != nil {
+			context.Errorf("Error parsing start:%s:%v", startParam, err)
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		} else {
+			duration = end.Sub(start)
+		}
+	}
+
 	// read aggregates
-	aggregates, err := readAggregates(context, boardKey, namespace, binType, time.Now(), 0, 0, "")
+	aggregates, err := readAggregates(context, boardKey, namespace, binType, time.Now(), duration, 0, "")
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusBadRequest)
 		return

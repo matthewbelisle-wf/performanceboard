798cfe9ade91a0b22b7ce0e54eddbebb61843c3b
Chad Bohannan <chad.bohannan@webfilings.com>
2014-08-14 17:35:33 -0600

    Added counts as stack plot.

--- client/src/graph_agg/directive.js
+++ client/src/graph_agg/directive.js
@@ -13,42 +13,71 @@ var directive = function(
         var seriesMap = {
             max: {
                 data: [],
+                renderer: 'line',
                 name: 'max',
                 color: 'red'
             },
             mean: {
                 data: [],
+                renderer: 'line',
                 name: 'mean',
                 color: 'green'
             },
             min: {
                 data: [],
+                renderer: 'line',
                 name: 'min',
                 color: 'blue'
             },
+            count: {
+                data: [],
+                renderer: 'stack',
+                name: 'counts',
+                color: 'grey'
+            },
         };
+
+        var min = -1;
+        var max = -1;
+        var maxCount = 0;
 
         $.each(binStats, function(i, binStat) {
             xLabels.push(binStat.start);
             var start = Date.parse(binStat.start) / 1000;
             var x = binStats.length - i - 1;
+            if(min === -1 || min > binStat.min)
+                min = binStat.min;
+            if(max === -1 || max < binStat.max)
+                max = binStat.max;
+            if(maxCount < binStat.count)
+                maxCount = binStat.count;
 
             seriesMap.min.data.unshift({x: x, y: binStat.min});
             seriesMap.mean.data.unshift({x: x, y: binStat.mean});
             seriesMap.max.data.unshift({x: x, y: binStat.max});
+            seriesMap.count.data.unshift({x: x, y: binStat.count});
         });
+
+        var scale = d3.scale.linear().domain([0, max]).nice();
+        seriesMap.max.scale = scale;
+        seriesMap.mean.scale = scale;
+        seriesMap.min.scale = scale;
+
+        var scale2 = d3.scale.linear().domain([0, maxCount]).nice();
+        seriesMap.count.scale = scale2;
 
         var series = 
         [
-            seriesMap.min, 
+            seriesMap.count,
+            seriesMap.max,
             seriesMap.mean,
-            seriesMap.max
+            seriesMap.min 
         ];
 
         var graph = new Rickshaw.Graph({
             element: $('#graph-' + index).get(0),
             height: 400,
-            renderer: 'line',
+            renderer: 'multi',
             series: series
         });
 
@@ -63,14 +92,35 @@ var directive = function(
             tickOffsetX: -10,
         });
 
-        new Rickshaw.Graph.Axis.Y({
-            graph: graph,
-            ticksTreatment: 'glow',
-            tickFormat: function(y) { return y + 'ms'; }
+        // TODO get rid of this
+        // new Rickshaw.Graph.Axis.Y({
+        //     graph: graph,
+        //     ticksTreatment: 'glow',
+        //     tickFormat: function(y) { return y + 'ms'; }
+        // });
+
+        // TODO hang this on an HTML element
+        new Rickshaw.Graph.Axis.Y.Scaled({
+          graph: graph,
+          orientation: 'right',
+          scale: scale,
+          tickFormat: function(y) { return y + 'ms'; }
         });
+
+        // TODO hang this on an HTML element
+        // new Rickshaw.Graph.Axis.Y.Scaled({
+        //   graph: graph,
+        //   orientation: 'right',
+        //   scale: scale2,
+        // });
 
         new Rickshaw.Graph.Axis.Time({
           graph: graph
+        });
+
+        new Rickshaw.Graph.Legend({
+            graph: graph,
+            element: $('#legend-' + index).get(0),
         });
 
         new Rickshaw.Graph.HoverDetail({
